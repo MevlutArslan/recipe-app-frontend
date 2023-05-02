a6376a93d6a1063f4ce62c647bd1d1a95e7da911
MevlutArslan <mevlut12098@gmail.com>
2023-05-02 18:33:03 -0500

    implement ability to fetch recipe by recipe id

--- recipe-app/Models/Recipe/RecipeModel.swift
+++ recipe-app/Models/Recipe/RecipeModel.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class RecipeModel {
     // @escaping makes sure we wait for the results to be ready before we return to make sure we don't return an empty array.
-    
-    func fetchRecipes(completion: @escaping ([Recipe]) -> Void) {
-        guard let url = URL(string: "http://10.0.0.62:8888/recipes") else {
+    private static let baseUrl = "http://10.0.0.62:8888/"
+    static func fetchRecipes(completion: @escaping ([Recipe]) -> Void) {
+        guard let url = URL(string: baseUrl + "recipes") else {
             completion([])
             return
         }
+        
         var request = URLRequest(url: url)
         request.cachePolicy = .returnCacheDataElseLoad
         
@@ -52,7 +53,7 @@ class RecipeModel {
         task.resume()
     }
     
-    func fetchRecipeImage(imageUrl: String, completion: @escaping ((Result<UIImage, Error>) -> Void)) {
+    static func fetchRecipeImage(imageUrl: String, completion: @escaping ((Result<UIImage, Error>) -> Void)) {
         guard let url = URL(string: imageUrl) else {
             completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
             return
@@ -68,6 +69,40 @@ class RecipeModel {
         }
         
         task.resume()
+    }
+    
+    static func fetchRecipeById(recipeId: String, completion: @escaping ((Result<Recipe, Error>) -> Void)) {
+        guard let url = URL(string: baseUrl + "recipe/\(recipeId)") else {
+            completion(.failure(NSError(domain: "Failed to create URL", code: 0)))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else {
+                print(error!)
+                completion(.failure(NSError(domain: "Failed to connect to URL", code: 0)))
+                return
+            }
+            
+            // handle response
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NSError(domain: "Unexpected response status code", code: 0)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data Received", code: 0)))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let recipe = try decoder.decode(Recipe.self, from: data)
+                completion(.success(recipe))
+            } catch let error {
+                completion(.failure(error))
+            }
+        }
     }
 
 
