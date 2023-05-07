c7e63388bfe042a4f4fd88d6b9bbb8152fdebc65
MevlutArslan <mevlut12098@gmail.com>
2023-05-07 15:25:13 -0500

    implement basic ARScene

--- recipe-app/AR/ARViewController.swift
+++ recipe-app/AR/ARViewController.swift
@@ -9,36 +9,43 @@ import UIKit
 import ARKit
 
 class ARViewController: UIViewController {
+    var arView: ARSCNView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                // Create a new ARSCNView instance
+        arView = ARSCNView(frame: self.view.frame)
+
+        // Add the ARSCNView to your view hierarchy
+        self.view.addSubview(arView)
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let worldTrackingConfig = ARWorldTrackingConfiguration()
-        worldTrackingConfig.planeDetection = [.vertical, .horizontal]
-
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGray
-        // Do any additional setup after loading the view.
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
         
+        // Run the AR session
+        arView.session.run(configuration)
     }
+    
     
     
     func setupView() {
         
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
