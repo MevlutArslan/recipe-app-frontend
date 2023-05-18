f8f0b064def78e71c2c5002d1b593bcf749de0d2
MevlutArslan <mevlut12098@gmail.com>
2023-05-18 13:39:27 -0500

    replace SceneKit with Reality Kit and add bunch of helper functions

--- recipe-app/AR/ARViewController+ARSessionDelegate.swift
+++ recipe-app/AR/ARViewController+ARSessionDelegate.swift
@@ -5,27 +5,33 @@
 //  Created by Mevlut Arslan on 09/05/2023.
 //
 import ARKit
+import RealityKit
 import Vision
 import AVFoundation
 
+enum Direction {
+    case TowardsUser
+    case FacingUp
+}
+
 extension ARViewController: ARSessionDelegate {
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
-
+        
         let imageBuffer = frame.capturedImage
         // Pass imageBuffer to Vision for processing
         var fingerTips: [CGPoint] = []
-
+        
         let ciImage = CIImage(cvPixelBuffer: imageBuffer)
-
+        
         // Get the image from the sample buffer
         let handler = VNImageRequestHandler(ciImage: ciImage)
-
+        
         do {
             try handler.perform([handPoseRequest])
             guard let results = handPoseRequest.results else {
                 return
             }
-
+            
             // Clear previously drawn points
             DispatchQueue.main.async {
                 // Remove all circle layers from view
@@ -33,15 +39,15 @@ extension ARViewController: ARSessionDelegate {
                     $0.removeFromSuperlayer()
                 }
             }
-
+            
             var recognizedPoints: [VNRecognizedPoint] = []
             try results.forEach { observation in
                 // Get finger points
                 let fingers = try observation.recognizedPoints(.all)
-
+                
                 recognizedPoints.append(contentsOf: fingers.values)
             }
-
+            
             // Filter recognized points and convert to view coordinates
             fingerTips = recognizedPoints.filter {
                 // Ignore low confidence points.
@@ -50,10 +56,10 @@ extension ARViewController: ARSessionDelegate {
             .map {
                 return VNImagePointForNormalizedPoint(CGPoint(x: $0.location.y, y: $0.location.x), Int(self.arView.bounds.width),  Int(self.arView.bounds.height))
             }
-
-
+            
+            
             guard fingerTips.count > 0 else {return}
-
+            
             // Draw circles at the fingertip locations
             let path = UIBezierPath()
             DispatchQueue.main.async {
@@ -61,17 +67,58 @@ extension ARViewController: ARSessionDelegate {
                     path.move(to: point)
                     path.addArc(withCenter: point, radius: 5, startAngle: 0, endAngle: 360, clockwise: true)
                 }
-
+                
                 let circleLayer = CAShapeLayer()
                 circleLayer.path = path.cgPath
                 circleLayer.fillColor = UIColor.red.cgColor
                 self.view.layer.addSublayer(circleLayer)
             }
         }
-     catch {
-        print("Error: \(error)")
+        catch {
+            print("Error: \(error)")
+        }
+        
     }
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        anchors.forEach {
+            guard let imageAnchor = $0 as? ARImageAnchor else { return }
+            
+            let customAnchor = AnchorEntity(anchor: imageAnchor)
+            
+            let box = MeshResource.generateBox(size: 0.016)
+            let material = SimpleMaterial(color: .gray, isMetallic: false)
+            let diceEntity = ModelEntity(mesh: box, materials: [material])
+            let imageFacingDirection: Direction = imageFacingDirection(anchor: imageAnchor)
+            
+            if imageFacingDirection == Direction.TowardsUser {
+//                generateFrame()
+            } else {
+//                generateStandingFrame()
+            }
+            
+            customAnchor.addChild(diceEntity)
+            self.arView.scene.addAnchor(customAnchor)
+        }
+    }
+  
+    func imageFacingDirection(anchor: ARAnchor) -> Direction{
+        let cameraRotation = self.arView.session.currentFrame?.camera.eulerAngles
+        
+        let anchorTransform = anchor.transform
+        
+        let worldCameraRotation = simd_quatf(angle: cameraRotation!.y, axis: SIMD3<Float>(0, 1, 0))
+        let anchorToWorldTransform = simd_inverse(anchorTransform)
+        let anchorRotation = anchorToWorldTransform.columns.0
+        let anchorRotationInWorld = simd_quatf(vector: anchorRotation)
+        
+        let dotProduct = simd.dot(worldCameraRotation.act(simd_make_float3(0, 0, -1)), anchorRotationInWorld.act(simd_make_float3(0, 1, 0)))
 
-}
-
+        if dotProduct > 0 {
+            // Y-axis is pointing up
+            return Direction.FacingUp
+        }
+        return Direction.TowardsUser
+    }
+    
 }
--- recipe-app/AR/ARViewController.swift
+++ recipe-app/AR/ARViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import ARKit
 import Vision
 import AVFoundation
+import RealityKit
 
 enum ARSessionStates {
     case QRCodeDisplayed
@@ -17,14 +18,15 @@ enum ARSessionStates {
     case ARSessionStarted
 }
 
-class ARViewController: UIViewController, ARSCNViewDelegate {
-    private(set) var arView: ARSCNView!
-    private(set) var arSession = ARSession()
-    private var loadedImage: UIImage = UIImage()
+class ARViewController: UIViewController {
+    private(set) var arView: ARView!
+    private(set) var previousViewController: RecipeViewController?
+
+    private var loadedQRCode: UIImage = UIImage()
     private var state: ARSessionStates = .QRCodeDisplayed
-    private let configuration = ARWorldTrackingConfiguration()
-    private var previousViewController: RecipeViewController?
-    
+
+    let configuration = ARWorldTrackingConfiguration()
+
     let handPoseRequest: VNDetectHumanHandPoseRequest = {
         let request = VNDetectHumanHandPoseRequest()
         request.maximumHandCount = 2
@@ -33,35 +35,32 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        // Create a session configuration
+
         configuration.worldAlignment = .gravity
         configuration.maximumNumberOfTrackedImages = 1
+        configuration.frameSemantics.insert(.personSegmentationWithDepth)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        arView = ARSCNView(frame: self.view.frame)
-        arView.delegate = self
-        arView.session = arSession
-        arView.automaticallyUpdatesLighting = true
-        arView.autoenablesDefaultLighting = true
-        arView.showsStatistics = true
-        arView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
-        
+        arView = ARView(frame: self.view.frame)
+       
+        arView.debugOptions = [.showFeaturePoints, .showWorldOrigin, .showAnchorOrigins]
+        arView.session.delegate = self;
         self.view.addSubview(arView)
         
         ARModel.fetchSessionQRCode(completion: { imageResult in
             switch imageResult {
             case .success(let image):
-                self.loadedImage = image
+                self.loadedQRCode = image
             case .failure(let error):
                 print("Error fetching qr code: \(error.localizedDescription)")
             }
             
             DispatchQueue.main.async {
                 self.state = .QRCodeDisplayed
-                self.arView.addSubview(QrCodeMenuView(frame: self.arView.frame, qrCode: self.loadedImage, viewController: self))
+                self.arView.addSubview(QrCodeMenuView(frame: self.arView.frame, qrCode: self.loadedQRCode, viewController: self))
             }
         })
         
@@ -70,7 +69,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     }
     
     @objc func handleQRCodeDownload(tapGestureRecognizer: UITapGestureRecognizer) {
-        let activityViewController = UIActivityViewController(activityItems: [self.loadedImage], applicationActivities: nil)
+        let activityViewController = UIActivityViewController(activityItems: [self.loadedQRCode], applicationActivities: nil)
         present(activityViewController, animated: true)
         
         self.state = .QRCodeDownloaded
@@ -85,68 +84,16 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     }
     
     private func addQRCodeToTracking() {
-        guard let image = loadedImage.cgImage else {
+        guard let image = loadedQRCode.cgImage else {
             print("error accessing the qr code!")
             return
         }
         let referanceImage = ARReferenceImage(image, orientation: .up, physicalWidth: 0.035)
         
-        configuration.detectionImages.insert(referanceImage)
+        self.configuration.detectionImages.insert(referanceImage)
         
         self.state = .ARImageAdded
-        self.arSession.run(configuration)
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        // Check if the anchor is an image anchor
-        print("RENDERING")
-        guard let imageAnchor = anchor as? ARImageAnchor else {
-            return
-        }
-
-        let physicalWidth = imageAnchor.referenceImage.physicalSize.width
-        let physicalHeight = imageAnchor.referenceImage.physicalSize.height
-        
-        let widthInPixels = self.getDimensionInPixels(meters: Float(physicalWidth))
-        let heightInPixels = self.getDimensionInPixels(meters: Float(physicalHeight))
-        
-        let frameWidthInPixels: Float = 390
-        let frameHeightInPixels: Float = 844
-
-        let frameWidthInMeters: Float = self.pixelToMeters(pixels: frameWidthInPixels)
-        let frameHeightInMeters: Float = self.pixelToMeters(pixels: frameHeightInPixels)
-        
-        let cube = SCNBox(width: CGFloat(frameWidthInMeters), height: CGFloat(frameHeightInMeters), length: CGFloat(frameWidthInMeters) / 3, chamferRadius: 0)
-        
-        // Create a plane geometry to visualize the initial position of the detected image
-        let mainPlane = SCNPlane(width: CGFloat(frameWidthInMeters), height: CGFloat(frameHeightInMeters))
-        
-        let mainNode = SCNNode(geometry: mainPlane)
-        mainNode.position = SCNVector3(x: 0, y: 0, z: ((frameWidthInMeters / 3) / 2) + 0.0012)
-        mainNode.opacity = 1
-        
-        DispatchQueue.main.async {
-            let recipeView = RecipeView(frame: self.arView.frame, recipe: self.previousViewController!.recipe!)
-
-            mainPlane.firstMaterial?.diffuse.contents = recipeView.renderToImage()
-        }
-        
-        let cubeNode = SCNNode(geometry: cube)
-        cubeNode.position = SCNVector3(x: 0, y: 0, z: 0) // Position the cube slightly above the detected image
-        cubeNode.eulerAngles.x = -.pi / 2
-        cubeNode.addChildNode(mainNode)
-        node.transform = SCNMatrix4(imageAnchor.transform)
-        
-        node.addChildNode(cubeNode)
-        
-    }
-    
-    private func pixelToMeters(pixels: Float) -> Float {
-        return (pixels / 460) * 0.0254;
-    }
-    
-    private func getDimensionInPixels(meters: Float) -> Float {
-        return meters * 460 / 0.0254
+        self.arView.session.run(self.configuration)
     }
     
 }
